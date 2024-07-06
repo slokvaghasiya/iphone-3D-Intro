5cb2027dd06de773ee794e17d58fbe206bc94a88
Ishika Vaghasiya <slokvaghasiya676@gmail.com>
2024-07-06 17:45:06 +0530

    [ Make Some Changes in index file ]

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "./components/Index";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 
 const App = () => {
 
   return (
     <div className='App'>
+      <Navbar />
       <Router>
         <Routes>
-          <Route exact path='/iphone-3D-Intro' element={<Index />} />
+          <Route path='/iphone-3D-Intro/' element={<Index />} />
         </Routes>
       </Router>
+      <Footer />
     </div>
   )
 }
