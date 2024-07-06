c0de79fbe6db0f52ccdb333bce734f32b8769462
Ishika Vaghasiya <slokvaghasiya676@gmail.com>
2024-07-06 18:25:31 +0530

    [ Make Some Changes in vite.config ]

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "./components/Index";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 
 
 const App = () => {
 
   return (
     <div className='App'>
-      <Navbar />
       <Router>
         <Routes>
-          <Route path='/iphone-3D-Intro/' element={<Index />} />
+          <Route  path='/iphone-3D/' element={<Index />} />
         </Routes>
       </Router>
-      <Footer />
     </div>
   )
 }
--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,21 +1,21 @@
-
+import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Highlight from "./Highlight";
 import Model from "./Model";
 import Features from "./Features";
 import HowItWorks from "./HowItWorks";
-
+import Footer from "./Footer";
 
 const Index = () => {
   return (
     <div className="bg-black" >
-
+      <Navbar />
       <Hero />
       <Highlight />
       <Model />
       <Features />
       <HowItWorks />
-
+      <Footer />
     </div>
   )
 }
